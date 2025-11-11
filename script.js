b3266adf1626d34a220def028d72b8ed93d7558d
nathanscd <nathansec26@gmail.com>
2025-11-11 13:01:06 -0300

    Placeholder carousel

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ close.addEventListener('click', () => {
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+function showNextSlide() {
+  slides[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.add('active');
+}
+
+setInterval(showNextSlide, 5000);
