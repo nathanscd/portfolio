0a7a6b05eb880303707d75cdf1cba1ec7f8d76b7
nathanscd <nathansec26@gmail.com>
2025-11-11 11:06:26 -0300

    Style Change

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-  
+const menuBtn = document.getElementById('menu');
+const menu = document.getElementById('btn');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
