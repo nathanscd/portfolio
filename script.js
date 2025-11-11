735a056d33d1e215fb45769a956e1aa861fd0143
nathanscd <nathansec26@gmail.com>
2025-11-11 14:03:13 -0300

    First smoother scroll tryout

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ function showNextSlide() {
 }
 
 setInterval(showNextSlide, 5000);
+
+const lenis = new Lenis({
+  duration: 1.2, // control how smooth it feels
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // nice easing
+  smoothWheel: true,
+  smoothTouch: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
