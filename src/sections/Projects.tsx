cfca86583e7156cafec4da997e2dfcf45ab08fa5
Nathan <nathansec26@gmail.com>
2025-12-02 21:04:59 -0300

    Projects and style

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -12,14 +12,13 @@ animate={{ opacity: 1, y: 0 }}
 className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 px-6"
 id="card"
 >
-{items.map((p) => (
-<div key={p} className="bg-neutral-900 p-6 rounded-2xl">
-<h3 className="text-xl font-semibold mb-2">Projeto {p}</h3>
-<p className="opacity-70 text-sm">
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae atque quibusdam et? Nulla quidem odio excepturi mollitia cum, eligendi pariatur.
-</p>
+<div className="tech-box"> <h3 className="tech-title">Filtros</h3> <div className="tech-list"> <span>Tudo</span><h1></h1><span>HTML</span> <span>CSS</span> <span>JavaScript</span> <span>TypeScript</span> <span>React</span> <span>React Native</span> <span>Node.js</span> <span>Python</span> <span>SQL</span> <span>Figma</span> <span>Git</span> </div> </div>
+<div className="card">
+  <img src="src/static/GradeTracker.png"/>
+  <h1 className="title-project">GradeTracker</h1>
+  <h2 className="tech">React, TypeScript</h2>
+  <p className="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui sequi ullam quia dolores ea animi dolor unde officiis ipsa distinctio?</p>
 </div>
-))}
 </motion.section>
 );
 }
